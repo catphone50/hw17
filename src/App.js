26ab152e8e79ac51492e5246ecb1a1294c860902
catphone50 <catphone50@gmail.com>
2024-10-03 15:20:15 +0200

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import {
+  ThemeProvider,
+  createTheme,
+  CssBaseline,
+  Typography,
+} from "@mui/material";
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.palette.primary.main};
+  color: ${({ theme }) => theme.palette.text.secondary};
+  border-radius: 4px;
+  padding: 10px 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.primary.dark};
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 30px;
+`;
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const lightTheme = createTheme({
+    palette: {
+      type: "light",
+      primary: {
+        main: "#3745e9",
+      },
+      background: {
+        default: "#f5f5f5",
+      },
+      text: {
+        primary: "#333",
+        secondary: "#dddddd",
+      },
+    },
+  });
+
+  const darkTheme = createTheme({
+    palette: {
+      type: "dark",
+      primary: {
+        main: "#93d4f9",
+      },
+      background: {
+        default: "#333",
+      },
+      text: {
+        primary: "#fff",
+        secondary: "#333",
+      },
+    },
+  });
+
+  function handelClick() {
+    setDarkMode(!darkMode);
+  }
+
+  const theme = darkMode ? darkTheme : lightTheme;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Typography variant="h2" align="center">
+          {darkMode ? "Dark Mode" : "Light Mode"}
+        </Typography>
+
+        <Button onClick={handelClick}>Toggle Theme</Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
